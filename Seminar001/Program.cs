4dd7e89da721ab44641b29c3eee0a21ffd865be7
Mikhail <koshkarev97@icloud.com>
2022-02-01 19:01:01 +0300

    Seminar 001

---
+++ Seminar001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести квадрат числа 
+
+//Выводим на консоль сообщение 
+
+Console.Write("Введите число - ");
+
+string numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr);
+
+Console.WriteLine($"Квадрат числа: {number * number}");
+
