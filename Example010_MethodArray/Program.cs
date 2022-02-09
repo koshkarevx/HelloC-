9042ceec7e4fba428248cd4c16562b58b9d5b455
Mikhail <koshkarev97@icloud.com>
2022-02-09 21:43:34 +0300

    Add Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿//              0   1   2   3    4   5   6   7   8   9
+int[] array = {11, 24, 53, 123, 53, 52, 75, 232, 75, 55};
+
+int n = array.Length;
+int find = 75;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+// index = index + 1;
+index++;
+}
