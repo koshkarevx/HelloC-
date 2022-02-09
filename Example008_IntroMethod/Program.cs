ecaa7d0a194d61e7754c86b5b129a3d0413e011d
Mikhail <koshkarev97@icloud.com>
2022-02-09 21:42:20 +0300

    Add Example008_IntroMethod

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -14,12 +14,14 @@ int b2 = 549;
 int c2 = 322;
 int a3 = 2342;
 int b3 = 1110;
-int c3 = 1513;
+int c3 = 15113;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
 
